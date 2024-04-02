ca7c0f2a00a0ff2a7c4bb2aacc01d278554f2004
vinay961 <vinay100rai@gmail.com>
2024-04-02 12:11:11 +0530

    controller for getting user details

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -291,6 +291,66 @@ const updateUserCoverImage = asyncHandler(async(req,res) => {
     )
     return res.status(200).json(200,user,"CoverImage updated successfully.")
 })
+
+const getUserChannelProfile = asyncHandler(async (req,res) => {
+    const {username} = req.params
+
+    if(!username){
+        throw new ApiError(400,"username is missing.")
+    }
+
+    const channel = User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount:{
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    if: {$in: [req.user?._id , "$subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subsccribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+
+    ])
+})
 export {
     registerUser,
     loginUser,
